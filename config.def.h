b4bb2fe5c4a7478f7139b2fcbb9d7643a8cb3696
ZigZag35 <rayenpilote0@gmail.com>
2023-01-28 22:57:53 +0100

    font size change

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hermit:style=Regular:size=11";
+static char *font = "Hermit:style=Regular:size=10";
 
 static int borderpx = 0;
 
